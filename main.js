455b1fe1d331965be5ccf674513a9c7b6433d2f8
Vanesa <vaneebg@gmail.com>
2022-05-04 17:25:01 +0200

    quitar

--- main.js
+++ main.js
@@ -8,14 +8,10 @@ let myModal = new bootstrap.Modal(document.getElementById('myModal'), {});
 
 // function abrir() {
 
-<<<<<<< HEAD
-getA.addEventListener("click", abrir)
-=======
 //     myModal.show();
 // }
 
 // getA.addEventListener("click",abrir)
 
 // funcion anonima :s 
-getA.addEventListener("click", () => myModal.show() )
->>>>>>> 256819a7176c0a746cb608b0dd56752ca09106c7
+getA.addEventListener("click", () => myModal.show())
