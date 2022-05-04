41ee05ca67155dbf51fa7f61e884c63ac5408442
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-05-04 17:23:05 +0200

    iniciando el javascript

--- main.js
+++ main.js
@@ -3,19 +3,28 @@
 const getA = document.getElementById('cardA')
 let myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {});
 
+const home = document.getElementById('home')
+const quiz = document.getElementById('quiz')
+
 // esto se cambia mas tarde porque no quiero cerrrarlo
 const closeButton = document.getElementById('buttonNext')
 
+const buttonStart = document.getElementById('buttonStart')
 
+
+
+
+const startQuiz =() =>{
+    home.classList.add('d-none')
+    quiz.classList.remove('d-none')
+} 
+buttonStart.addEventListener('click',startQuiz  )
 
 
 // function abrir() {
-
 //     myModal.show();
 // }
-
 // getA.addEventListener("click",abrir)
-
 // funcion anonima :s 
 getA.addEventListener("click", () => myModal.show() )
 
