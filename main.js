92c2a06efe77c3f751fd7b3382b01969025036ae
Vanesa <vaneebg@gmail.com>
2022-05-08 17:00:49 +0200

    hecho readme inicial

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ const results = document.getElementById("results");
 const buttonNext = document.getElementById("buttonNext");
 const buttonStart = document.getElementById("buttonStart");
 const myModal = new bootstrap.Modal(
-  document.getElementById("staticBackdrop"),
-  {}
+    document.getElementById("staticBackdrop"), {}
 );
 const questionTitle = document.getElementById("question");
 const answerOptions = document.getElementById("answerOptions");
@@ -14,39 +13,39 @@ let currentQuestionIndex;
 let rightAnswers = 0;
 
 function hideView() {
-  home.classList.add("d-none");
-  quiz.classList.add("d-none");
-  results.classList.add("d-none");
+    home.classList.add("d-none");
+    quiz.classList.add("d-none");
+    results.classList.add("d-none");
 }
 
 function openModal() {
-  myModal.show();
-} 
+    myModal.show();
+}
 
 function closeModal() {
-  myModal.hide();
-} 
+    myModal.hide();
+}
 const setStatusClass = (cardElement, cardValue) => {
-  if (cardValue) {
-    cardElement.children[0].className = "card bg-success";
-    // element.children[0].classList.add("border-5")
-  } else {
-    cardElement.children[0].className = "card bg-danger";
-  }
+    if (cardValue) {
+        cardElement.children[0].className = "card bg-success";
+        // element.children[0].classList.add("border-5")
+    } else {
+        cardElement.children[0].className = "card bg-danger";
+    }
 };
-  
+
 // pendiente
 // if (card.correct == true) {
 //     rightAnswers++
 //   }
 
-const showQuestion = (currentQuestion) => { 
-console.log(currentQuestionIndex)
-  questionTitle.innerHTML = ` ${currentQuestion[0]} ?`;
- 
-  currentQuestion[1].forEach((answer) => {
-    const card = document.createElement("card");
-    card.innerHTML = ` 
+const showQuestion = (currentQuestion) => {
+    console.log(currentQuestionIndex)
+    questionTitle.innerHTML = ` ${currentQuestion[0]} ?`;
+
+    currentQuestion[1].forEach((answer) => {
+        const card = document.createElement("card");
+        card.innerHTML = ` 
                             <div class="card" id="card" >
                                 <img src="/Assets/1.jpg" class="card-img-top" alt="..." />
                                 <div class="card-body text-center">
@@ -55,80 +54,78 @@ console.log(currentQuestionIndex)
                             </div>            
                       `;
 
-    card.addEventListener("click", function selectAnswer() {
-        Array.from(answerOptions.children).forEach((card) => {
-          setStatusClass(card, card.dataset.correct);
+        card.addEventListener("click", function selectAnswer() {
+            Array.from(answerOptions.children).forEach((card) => {
+                setStatusClass(card, card.dataset.correct);
+            });
+            openModal();
         });
-         openModal();
-      }
-    );
-    answerOptions.appendChild(card);
-  });
+        answerOptions.appendChild(card);
+    });
 
-  // añade el atributo correcto a la primera card (que sabemos es correcta)
-  answerOptions.firstElementChild.dataset.correct = true;
+    // añade el atributo correcto a la primera card (que sabemos es correcta)
+    answerOptions.firstElementChild.dataset.correct = true;
 };
 const resetState = () => {
-  closeModal();
-  while (answerOptions.firstChild) {
-    answerOptions.removeChild(answerOptions.firstChild);
-  }
+    closeModal();
+    while (answerOptions.firstChild) {
+        answerOptions.removeChild(answerOptions.firstChild);
+    }
 };
 const nextQuestion = (data) => {
-  resetState();
-  showQuestion(data[currentQuestionIndex]);
-  
+    resetState();
+    showQuestion(data[currentQuestionIndex]);
+
 };
 
 // COGER DATOS DE LA API
-const questionsAPI = async () => {
-  const arrayAPI = await axios.get(
-    "https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple"
-  );
-  // console.log(arrayAPI.data.results)
-  let arrayQuestions = [];
+const questionsAPI = async() => {
+    const arrayAPI = await axios.get(
+        "https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple"
+    );
+    // console.log(arrayAPI.data.results)
+    let arrayQuestions = [];
 
-  // depuramos las preguntas.
-  arrayAPI.data.results.forEach((element) => {
-    const {
-      correct_answer: correctAnswer,
-      question,
-      incorrect_answers: incorrectAnswersArray,
-    } = element;
-    const answers = [correctAnswer, ...incorrectAnswersArray];   
-    const questionClear = question
-      .replaceAll(/&quot;/gi, "")
-      .replaceAll(/&#039;/gi, "");
-    const arrayQuestions1 = [questionClear, answers];
-    arrayQuestions.push(arrayQuestions1);
-  });
-  return arrayQuestions;
+    // depuramos las preguntas.
+    arrayAPI.data.results.forEach((element) => {
+        const {
+            correct_answer: correctAnswer,
+            question,
+            incorrect_answers: incorrectAnswersArray,
+        } = element;
+        const answers = [correctAnswer, ...incorrectAnswersArray];
+        const questionClear = question
+            .replaceAll(/&quot;/gi, "")
+            .replaceAll(/&#039;/gi, "");
+        const arrayQuestions1 = [questionClear, answers];
+        arrayQuestions.push(arrayQuestions1);
+    });
+    return arrayQuestions;
 };
-// const arrayQuestions = []
-questionsAPI().then((data) => {
-  console.log("listado preguntas",data)
-  
-  // aqui metemos funciones que necesiten esos datos
-  buttonStart.addEventListener("click", (e) => {
-    e.preventDefault();
-    hideView();
-    quiz.classList.remove("d-none");
-    currentQuestionIndex = 0;
-    nextQuestion(data);
-  });
 
-  buttonNext.addEventListener("click", () => {
-    if(data.length > currentQuestionIndex + 1){
-        currentQuestionIndex++;
-        nextQuestion(data)
-    }
-    else{
-        closeModal();
+questionsAPI().then((data) => {
+    console.log("listado preguntas", data)
+
+    // aqui metemos funciones que necesiten esos datos
+    buttonStart.addEventListener("click", (e) => {
+        e.preventDefault();
         hideView();
-        results.classList.remove("d-none")
-    }
-    
-})
+        quiz.classList.remove("d-none");
+        currentQuestionIndex = 0;
+        nextQuestion(data);
+    });
+
+    buttonNext.addEventListener("click", () => {
+        if (data.length > currentQuestionIndex + 1) {
+            currentQuestionIndex++;
+            nextQuestion(data)
+        } else {
+            closeModal();
+            hideView();
+            results.classList.remove("d-none")
+        }
+
+    })
 
 });
 // Cuando llamas a una función el parámetro tiene que ser algo tangible, por ej. un dato o un numero. Sin embargo, cuando se invoca el parametro es un nombre inventado cualquiera.(jaja)
