57c2118365bffaa1d15352d3168cdb9a820ab247
Vanesa <vaneebg@gmail.com>
2022-05-08 20:46:19 +0200

    resuelto conflicto en html

--- main.js
+++ main.js
@@ -8,11 +8,7 @@ const myModal = new bootstrap.Modal(
 );
 const questionTitle = document.getElementById("question");
 const answerOptions = document.getElementById("answerOptions");
-<<<<<<< HEAD
 const modalResponse = document.getElementById("modalResponse")
-=======
-const cardAnswer = document.getElementById("cardAnswer")
->>>>>>> 6b5ecb70a4e78deab6030326c9761a4b10b0518e
 let currentQuestionIndex;
 let rightAnswers = 0;
 
@@ -23,7 +19,6 @@ function hideView() {
     results.classList.add("d-none");
 }
 
-<<<<<<< HEAD
 function openModal() {
     myModal.show();
 }
@@ -31,37 +26,6 @@ function openModal() {
 function closeModal() {
     myModal.hide();
 }
-=======
-function openModal(answer) {
-    myModal.show();  
-    cardAnswer.innerText = `${answer[1][0]}`
-} 
-
-function closeModal() {
-  myModal.hide();
-} 
-
-
-// funcion para desordenar el array
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
-
->>>>>>> 6b5ecb70a4e78deab6030326c9761a4b10b0518e
 const setStatusClass = (cardElement, cardValue) => {
     if (cardValue) {
         cardElement.children[0].className = "card bg-success";
@@ -70,7 +34,6 @@ const setStatusClass = (cardElement, cardValue) => {
         cardElement.children[0].className = "card bg-danger";
     }
 
-<<<<<<< HEAD
 };
 
 // pendiente
@@ -83,21 +46,6 @@ const showQuestion = (currentQuestion) => {
     currentQuestion[2].forEach((answer) => {
         const card = document.createElement("card");
         card.innerHTML = ` 
-=======
-const showQuestion = (currentQuestion) => { 
-  questionTitle.innerHTML = ` ${currentQuestion[0]} ?`;
-// console.log("respuestas originales", currentQuestion[1])
-//   answerOptions.firstElementChild.dataset.correct = true;
-//   const randomQuestion = shuffle(currentQuestion[1])
-//   console.log("respuestas aleatorias", randomQuestion)
-
- 
-currentQuestion[1].forEach((answer) => {
-const card = document.createElement("card");
-   
-
-    card.innerHTML = ` 
->>>>>>> 6b5ecb70a4e78deab6030326c9761a4b10b0518e
                             <div class="card" id="card" >
                                 <img src="/Assets/1.jpg" class="card-img-top" alt="..." />
                                 <div class="card-body text-center">
@@ -116,7 +64,6 @@ const card = document.createElement("card");
                 rightAnswers++
             }
         });
-<<<<<<< HEAD
         answerOptions.appendChild(card);
     });
 
@@ -126,31 +73,6 @@ const card = document.createElement("card");
     })
 
 
-=======
-         openModal(currentQuestion);
-      }
-    );
-    answerOptions.appendChild(card);
-  });
-
-  // añade el atributo correcto a la primera card (que sabemos es correcta)
-  answerOptions.firstElementChild.dataset.correct = true;
-
-// //   ¿es muy loco hacer aqui otro bucle para pegar las aleatorias?
-// // esto no termina de funcionar porque no las hace bien
-// const randomQuestion = shuffle(currentQuestion[1])
-// currentQuestion[1].forEach((answer) => {
-//     console.log(answer)
-//     card.innerHTML = ` 
-//     <div class="card" id="card" >
-//         <img src="/Assets/1.jpg" class="card-img-top" alt="..." />
-//         <div class="card-body text-center">
-//             <h5 class="card-title">${randomQuestion}</h5>
-//         </div>
-//     </div>            
-// `;
-// })
->>>>>>> 6b5ecb70a4e78deab6030326c9761a4b10b0518e
 };
 const resetState = () => {
     closeModal();
@@ -169,7 +91,6 @@ const questionsAPI = async() => {
         "https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple"
     );
 
-<<<<<<< HEAD
     let arrayQuestions = [];
 
     // depuramos las preguntas.
@@ -189,48 +110,9 @@ const questionsAPI = async() => {
         arrayQuestions.push(arrayQuestions1);
     });
     return arrayQuestions;
-=======
-  // depuramos las preguntas.
-  arrayAPI.data.results.forEach((element) => {
-    const {
-      correct_answer: correctAnswer,
-      question,
-      incorrect_answers: incorrectAnswersArray,
-    } = element;
-    const answers = [correctAnswer, ...incorrectAnswersArray];  
-    // console.log("respuestas",answers) 
-    // console.log("respuesta correcta", answers[0])
-    // answers[0].dataset.correct = true
-
-    const questionClear = question
-      .replaceAll(/&quot;/gi, "")
-      .replaceAll(/&#039;/gi, "");
-    const arrayQuestions1 = [questionClear, answers];
-    arrayQuestions.push(arrayQuestions1);
-  });
-
-  console.log(arrayQuestions)
-  return arrayQuestions;
->>>>>>> 6b5ecb70a4e78deab6030326c9761a4b10b0518e
 };
 
 questionsAPI().then((data) => {
-<<<<<<< HEAD
-=======
-//   console.log("listado preguntas",data)
-//   console.log("pregunta aleatoria:", data[0][0])
-//   console.log("total de respuestas de la pregunta aleatoria:", data[0][1])
-//   console.log("respuesta correcta de la pregunta aleatoria:", data[0][1][1])
-  
-  // aqui metemos funciones que necesiten esos datos
-  buttonStart.addEventListener("click", (e) => {
-    e.preventDefault();
-    hideView();
-    quiz.classList.remove("d-none");
-    currentQuestionIndex = 0;
-    nextQuestion(data);
-  });
->>>>>>> 6b5ecb70a4e78deab6030326c9761a4b10b0518e
 
     buttonStart.addEventListener("click", (e) => {
         e.preventDefault();
